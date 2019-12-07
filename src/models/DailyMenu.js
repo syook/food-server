24d3179710f453fa7afde7dc7f43614b822fa79c
Nithil N <nithil@syook.com>
2019-12-07 12:10:37 +0530

    Added prettier

--- src/models/DailyMenu.js
+++ src/models/DailyMenu.js
@@ -1,29 +1,27 @@
-
 const db = require('../config/db');
 const Schema = require('mongoose').Schema;
 
 const DailyMenuSchema = new Schema(
   {
     date: {
-        type: Date,
-        default: Date.now,
+      type: Date,
+      default: Date.now
     },
     breakfastItems: {
-        type: [Schema.Types.ObjectId],
-        ref: 'FoodItem'
+      type: [Schema.Types.ObjectId],
+      ref: 'FoodItem'
     },
     lunchItems: {
-        type: [Schema.Types.ObjectId],
-        ref: 'FoodItem'
+      type: [Schema.Types.ObjectId],
+      ref: 'FoodItem'
     },
     usersData: {
-        type: Schema.Types.Mixed
+      type: Schema.Types.Mixed
     }
   },
   {
-    timestamps:true
+    timestamps: true
   }
 );
-
 
 module.exports = db.model('DailyMenu', DailyMenuSchema);
